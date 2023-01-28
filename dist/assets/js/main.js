bfef70c1b09ac98c093e766391cbc22bb129f585
Ato Dawit <dave5225@gmail.com>
2023-01-27 20:53:48 -0600

    code repair

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,8 +1,6 @@
 /**
-* Template Name: DevFolio - v4.9.1
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+* Name: DevFolio - v4.9.1
+*  
 */
 (function() {
   "use strict";
